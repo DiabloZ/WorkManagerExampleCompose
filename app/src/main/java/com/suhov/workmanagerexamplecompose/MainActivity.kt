9cab7de130afd17537c7e0e0d22b8c841bf1e6d7
Сухов Виталий <sukhov_v@tanukitech.ru>
2022-10-30 17:16:35 +0300

    Created interfaces

--- app/src/main/java/com/suhov/workmanagerexamplecompose/MainActivity.kt
+++ app/src/main/java/com/suhov/workmanagerexamplecompose/MainActivity.kt
@@ -1,43 +1,136 @@
 package com.suhov.workmanagerexamplecompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+import androidx.work.*
+import coil.compose.rememberImagePainter
 import com.suhov.workmanagerexamplecompose.ui.theme.WorkManagerExampleComposeTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+/*        val downloadRequest = PeriodicWorkRequestBuilder<DownloadWorker>(
+            Duration.ofHours(5)
+        )*/
+        val downloadRequest = OneTimeWorkRequestBuilder<DownloadWorker>()
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(
+                        NetworkType.CONNECTED
+                    )
+                    .build()
+            )
+            .build()
+        val colorFilterRequest = OneTimeWorkRequestBuilder<ColorFilterWorker>()
+            .build()
+        val workManager = WorkManager.getInstance(applicationContext)
+
         setContent {
             WorkManagerExampleComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    mainInterfaces(workManager, downloadRequest, colorFilterRequest)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @SuppressLint("UnrememberedMutableState")
+    @Composable
+    fun mainInterfaces(
+        workManager: WorkManager,
+        downloadRequest: OneTimeWorkRequest,
+        colorFilterRequest: OneTimeWorkRequest
+    ) {
+        val workInfos = workManager
+            .getWorkInfosForUniqueWorkLiveData("download")
+            .observeAsState()
+            .value
+        val downloadInfo = remember(key1 = workInfos) {
+            workInfos?.find { it.id == downloadRequest.id }
+        }
+        val filterInfo = remember(key1 = workInfos) {
+            workInfos?.find { it.id == colorFilterRequest.id }
+        }
+        val imageUri by derivedStateOf {
+            val downloadUri = downloadInfo?.outputData?.getString(WorkerKeys.IMAGE_URI)
+                ?.toUri()
+            val filterUri = filterInfo?.outputData?.getString(WorkerKeys.FILTER_URI)
+                ?.toUri()
+            filterUri ?: downloadUri
+        }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            imageUri?.let { uri ->
+                Image(
+                    painter = rememberImagePainter(
+                        data = uri
+                    ),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+            Button(
+                onClick = {
+                    workManager.beginUniqueWork(
+                        "download",
+                        ExistingWorkPolicy.KEEP,
+                        downloadRequest
+                    )
+                        .then(colorFilterRequest)
+                        .enqueue()
+                },
+                enabled = downloadInfo?.state != WorkInfo.State.RUNNING
+            ) {
+                Text(text = "Start download")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WorkManagerExampleComposeTheme {
-        Greeting("Android")
+            when(downloadInfo?.state){
+                WorkInfo.State.RUNNING -> Text(text = "Downloading...")
+                WorkInfo.State.SUCCEEDED -> Text(text = "Download succeeded")
+                WorkInfo.State.FAILED -> Text(text = "Download failed")
+                WorkInfo.State.CANCELLED -> Text(text = "Download cancelled")
+                WorkInfo.State.ENQUEUED -> Text(text = "Download enqueued")
+                WorkInfo.State.BLOCKED -> Text(text = "Download blocked")
+                null -> Text(text = "Download failed")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            when(filterInfo?.state){
+                WorkInfo.State.RUNNING -> Text(text = "Applying filter...")
+                WorkInfo.State.SUCCEEDED -> Text(text = "Filter succeeded")
+                WorkInfo.State.FAILED -> Text(text = "Filter failed")
+                WorkInfo.State.CANCELLED -> Text(text = "Filter cancelled")
+                WorkInfo.State.ENQUEUED -> Text(text = "Filter enqueued")
+                WorkInfo.State.BLOCKED -> Text(text = "Filter blocked")
+                null -> Text(text = "Filter failed")
+            }
+        }
     }
+
 }
